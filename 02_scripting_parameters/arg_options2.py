1fa137933d223dedfc5a12151ff276c8620badfa
pushtakio <alex@vaiolabs.com>
2019-10-17 21:56:08 +0300

    adding argparse script

--- 02_scripting_parameters/arg_options2.py
+++ 02_scripting_parameters/arg_options2.py
@@ -2,3 +2,9 @@
 import argparse
 
 p = argparse.ArgumentParser()
+
+p.add_argument('echo', help='echo the string you use here')
+p.add_argument('-v','--verbose', help='adds verbosibility',action='store_true')
+args = p.parse_args()
+
+print(args.echo)
