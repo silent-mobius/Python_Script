2ca348983fdfd4649ac8d8ee0c474267b507da20
pushtakio <alex@vaiolabs.com>
2019-10-18 05:11:40 +0300

    adding conditions python

--- 03_scripting_conditions/conditions.py
+++ 03_scripting_conditions/conditions.py
@@ -14,3 +14,46 @@ a = 33
 b = 200
 if b > a:
 print("b is greater than a") # you will get an error
+
+input()
+
+#if elif
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+input()
+# if elif else
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+input()
+
+#else
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+input()
+
+#Nested if
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
