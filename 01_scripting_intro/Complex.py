443eaba6bbd186daba596ad2a80bf8c9f0ed6fab
Silent-Mobius <alex@vaiolabs.com>
2019-12-10 02:28:27 +0200

    adding complex explanation

---
+++ 01_scripting_intro/Complex.py
@@ -0,0 +1,7 @@
+'''
+ Mathematically, a complex number (generally used in engineering) is a number of the form A+Bi 
+ where i is the imaginary number. Complex numbers have a real and imaginary part. 
+ Python supports complex numbers either by specifying the number in (real + imagJ) or 
+ (real + imagj) form or using a built-in method complex(x, y). See the following statements 
+ in Python Shell.
+ '''
