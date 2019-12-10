031fc6399de964127ae91fd5c5c350a526c8be7c
Silent-Mobius <alex@vaiolabs.com>
2019-12-10 02:28:36 +0200

    adding classes

---
+++ 01_scripting_intro/Classes.py
@@ -0,0 +1,10 @@
+'''
+The basic idea behind an object-oriented programming (OOP) is to combine both data
+and associated procedures (known as methods) into a single unit which operate on the data.
+Such a unit is called an object.
+Python is an object-oriented language, everything in Python is an object.We have already
+worked with some objects in Python, (See Python data type chapter) for example strings,
+lists are objects defined by the string and list classes which are available by
+default into Python. Let's declare two objects a string and a list and test their type
+with type() function.
+'''
