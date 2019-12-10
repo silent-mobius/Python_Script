80f637f0698ae3daa0ba4b0f36c912944f5cbb1f
Silent-Mobius <alex@vaiolabs.com>
2019-12-10 02:27:39 +0200

    adding string explanation

---
+++ 01_scripting_intro/Strings.py
@@ -0,0 +1,7 @@
+'''
+String literals in python are surrounded by either single quotation marks, or double quotation marks.
+
+'hello' is the same as "hello".
+
+You can display a string literal with the print() function:
+'''
