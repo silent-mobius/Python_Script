392e7d18dadf24b305e5f639800b3b8c0a8a9dee
Silent-Mobius <alex@vaiolabs.com>
2019-12-10 03:13:10 +0200

    adding variable explanation

---
+++ 01_scripting_intro/Variables.py
@@ -0,0 +1,5 @@
+'''
+Variables are containers for storing data values.
+Unlike other programming languages, Python has no command for declaring a variable.
+A variable is created the moment you first assign a value to it.
+'''
