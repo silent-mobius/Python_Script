25fc0a2534926ca6f6147da58b75f8b89f600362
Pushtakio <alex@vaiolabs.com>
2020-05-04 02:32:48 +0300

    adding function examples

--- 06_functions/Func.py
+++ 06_functions/Func.py
@@ -31,11 +31,25 @@ def my_useless_function2(fname="Alex", lname="Schapelle"):
 my_useless_function2('Arnold','Knoles') # if you don't get the joke seek for beyonces meme on 9gag
 
 
-# in case there is infinite arguments(AKA Arbitrary Arguments, *args)
+# in case there is infinite arguments and you wish it to work like List(AKA Arbitrary Arguments, *args)
 def my_counting_function( *stuff):
     for s in stuff:
         print(s)
 
 my_counting_function('stuff_1','stuff_2','stuff_3','stuff_4')
 
+# in case there is infinite arguments and you wish it to work like Dict(AKA Arbitrary Keyword Arguments, **kwargs)
+def greet_me(**kwargs):
+    if kwargs is not None:
+        for key, value in kwargs.iteritems():
+            print(f"{key}=={value}")
 
+greet_me(name="Yoda") # watch star war for Gods sake...
+
+## returning the value
+def count_func(x):
+      return 2 * x
+
+print(count_func(2))
+print(count_func(4))
+print(count_func(8))
