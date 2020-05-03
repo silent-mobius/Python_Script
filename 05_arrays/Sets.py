54998add7233442111f5ae6a561a8ebba8ad3b5a
Pushtakio <alex@vaiolabs.com>
2020-05-04 01:52:01 +0300

    adding set example

---
+++ 05_arrays/Sets.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+data=set()
+count = 5
+
+while len(data) < count:
+    var = int(input("Please insert number: "))
+    data.add(var)
+
+print("all Set data inserted: ", data)
