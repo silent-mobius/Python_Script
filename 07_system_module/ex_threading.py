4f661c09d75ab8724500993920449526f1d14d34
Pushtakio <alex@vaiolabs.com>
2020-05-05 11:24:38 +0300

    adding threading example

---
+++ 07_system_module/ex_threading.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# taken from https://www.simplifiedpython.net/python-threading-example/
+# another link for example https://realpython.com/intro-to-python-threading/
+''' # preview code for threading
+import time
+
+#First Method
+def greet_them(people):
+    for person in people:
+        print("Hello Dear " + person + ". How are you?")
+        time.sleep(0.5)
+
+#Second Method
+def assign_id(people):
+    i = 1
+    for person in people:
+        print("Hey! {}, your id is {}.".format(person, i))
+        i += 1
+        time.sleep(0.5)
+
+
+people = ['Richard', 'Dinesh', 'Elrich', 'Gilfoyle', 'Gevin']
+
+t = time.time()
+
+greet_them(people)
+assign_id(people)
+
+print("Woaahh!! My work is finished..")
+print("I took " + str(time.time() - t))
+
+'''
+##########################################################
+
+import time
+import threading
+
+# Python Threading Example for Beginners
+# First Method
+def greet_them(people):
+    for person in people:
+        print("Hello Dear " + person + ". How are you?")
+        time.sleep(0.5)
+
+
+# Second Method
+def assign_id(people):
+    i = 1
+    for person in people:
+        print("Hey! {}, your id is {}.".format(person, i))
+        i += 1
+        time.sleep(0.5)
+
+
+people = ['Richard', 'Dinesh', 'Elrich', 'Gilfoyle', 'Gevin']
+
+t = time.time()
+
+#Created the Threads
+t1 = threading.Thread(target=greet_them, args=(people,))
+t2 = threading.Thread(target=assign_id, args=(people,))
+
+#Started the threads
+t1.start()
+t2.start()
+
+#Joined the threads
+t1.join()
+t2.join()
+
+print("Woaahh!! My work is finished..")
+print("I took " + str(time.time() - t))
